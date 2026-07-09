f50f673b4cd805067265cc6f7e6a44c3e1bc656b
Riki <rmcal024@uottawa.ca>
2026-07-09 15:24:10 -0400

    Renamed BookButton to BookDropdown and added interactions to each dropdown item

--- src/components/BookDropdown.jsx
+++ src/components/BookDropdown.jsx
@@ -1,6 +1,6 @@
 import { addToBookshelf } from "../db/database.js";
 
-function BookButton({book}) {
+function BookDropdown({book}) {
 
     function addToBookshelfButton(status) {
         const handleAdd = async () => {
@@ -15,22 +15,22 @@ function BookButton({book}) {
 
     return(
         <>
-            <div className="input-group w-100">
-                <button onClick={() => addToBookshelfButton("want_to_read")} type="button" className="btn btn-primary flex-grow-1">Want to Read</button>
+            <div className="input-group">
+                <button onClick={() => addToBookshelfButton("want_to_read")} type="button" className="btn btn-primary ">Want to Read</button>
                 <button type="button"
-                        className="btn btn-primary dropdown-toggle dropdown-toggle-split border-1 border-white border-top-0 border-bottom-0 border-end-0"
+                        className="btn btn-primary dropdown-toggle dropdown-toggle-split"
                         data-bs-toggle="dropdown"
                         aria-expanded="false">
                     <span className="visually-hidden">Toggle Dropdown</span>
                 </button>
                 <ul className="dropdown-menu dropdown-menu-end">
-                    <li><a className="dropdown-item" href="#">Currently Reading</a></li>
-                    <li><a className="dropdown-item" href="#">Read</a></li>
-                    <li><a className="dropdown-item" href="#">Did Not Finish</a></li>
+                    <li><button onClick={() => addToBookshelfButton("reading")} className="dropdown-item" type="button">Currently Reading</button></li>
+                    <li><button onClick={() => addToBookshelfButton("read")} className="dropdown-item" type="button">Read</button></li>
+                    <li><button onClick={() => addToBookshelfButton("dnf")} className="dropdown-item" type="button">Did Not Finish</button></li>
                 </ul>
             </div>
         </>
     );
 }
 
-export default BookButton;
+export default BookDropdown;
